77b506e748fbcf8381603a751c6f326b30a3886e
Scott Main <smain@google.com>
2013-11-04 18:33:43 -0800

    fixes broken links
    Change-Id: If183318b5a61d752d07a0152bcfc6845025b876a

--- docs/html/samples/samples_toc.cs
+++ docs/html/samples/samples_toc.cs
@@ -1,4 +1,6 @@
+
 <ul id="nav" class="samples-nav">
+
 <script type="text/javascript">
 <!--
     buildToggleLists();
@@ -6,7 +8,7 @@
 //-->
 </script>
   <li class="nav-section">
-    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>samples/index.html">
+    <div class="nav-section-header empty"><a href="/samples/index.html">
       <span class="en">About the Samples</span></a>
     </div>
   </li>
